c7ba1a6a8c796dabb233f67b58c6205d7870cb4a
LeoFelixCruz <felixcruz@live.com>
2025-09-02 20:47:01 -0300

    'Ajustar no vscode'

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,5 +1,5 @@
 const { somarDoisNumeros } = require('../src/calculadora');
 
-const resultadoDaSoma = somarDoisNumeros(5, 3);
+const resultadoDaSoma = somarDoisNumeros(10, 5);
 
 console.log(resultadoDaSoma)
